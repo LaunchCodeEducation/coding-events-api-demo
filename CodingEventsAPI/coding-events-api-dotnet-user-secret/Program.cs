a7edf56444ebcf85410f23794050dbfca9bd9006
Kellie Kimble <kelliekimble@gmail.com>
2020-08-05 14:51:16 -0400

    trying

---
+++ CodingEventsAPI/coding-events-api-dotnet-user-secret/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coding_events_api_dotnet_user_secret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
